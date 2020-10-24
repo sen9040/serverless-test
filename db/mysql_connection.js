4dcfae6ec53dc403bdc2927f01556c65c47e89ab
sen9040 <62924580+sen9040@users.noreply.github.com>
2020-10-25 00:50:41 +0900

    db env name change

--- db/mysql_connection.js
+++ db/mysql_connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWD,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
 });
